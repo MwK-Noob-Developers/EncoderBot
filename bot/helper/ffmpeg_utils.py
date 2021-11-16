2e4290b2a46be92a85a8b257d9879b55537deeb9
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-11-16 22:13:11 +0530

    Create ffmpeg_utils.py

--- bot/helper/ffmpeg_utils.py
+++ bot/helper/ffmpeg_utils.py
@@ -40,7 +40,7 @@ def encode(filepath):
             video_opts = '-c:v copy -tag:v hvc1'
     else:
         # Transcode to h265 / hvc1
-        video_opts = '-c:v libx265 -metadata title=shamiLneLLi -crf 32 -tag:v hvc1 -preset slow -threads 8'
+        video_opts = '-c:v libx265 -crf 32 -tag:v hvc1 -preset slow -threads 8'
     # Get the audio channel codec
     audio_codec = get_codec(filepath, channel='a:0')
     if audio_codec == []:
